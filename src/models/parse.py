66fddc3251edf8e5f786adc37735af299a073568
Nuno Pereira <nunoafonso2002@gmail.com>
2023-03-08 23:31:14 +0000

    Factorized CSV parsing logic

--- src/models/parse.py
+++ src/models/parse.py
@@ -1,6 +1,17 @@
-from typing import List, TypeVar
+from typing import List, Dict, Any, Generator, TypeVar
 
 import csv
+
+def parse_file(file: str) -> Generator[List[Dict[str, Any]], None, None]:
+    """
+        Parses a CSV file and returns a list of the data contained in it
+    """
+
+    with open(file, 'r') as file:
+        reader = csv.DictReader(file, delimiter=',')
+
+        for line in reader:
+            yield line
 
 Model = TypeVar('Model')
 def parse_model(file: str, model: Model) -> List[Model]:
@@ -10,10 +21,7 @@ def parse_model(file: str, model: Model) -> List[Model]:
 
     models = []
 
-    with open(file, 'r') as file:
-        reader = csv.DictReader(file, delimiter=',')
-
-        for line in reader:
-            models.append(model(*line.values()))
+    for line in parse_file(file):
+        models.append(model(*line.values()))
 
     return models
